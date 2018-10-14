2e6da09335d8a64c4de484f76cb62827fbb6a051
hato-ryuji <t.i_neko-yunomi@rice.ocn.ne.jp>
2018-10-14 16:28:35 +0900

    王将と飛車のプレハブを作成

---
+++ Assets/Scripts/BaseKoma.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 駒のスクリプトのベースクラス
+/// </summary>
+public abstract class BaseKoma : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
